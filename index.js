6b300ebf6edf2e8d143b24918e35dfc8e93afbb7
Daniel Calla Castillo <danielcallacastillo7u7@gmail.com>
2026-05-07 17:58:20 -0500

    fix backend

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-import express, { application } from 'express'
-const app = express()
+import express from "express";
+
+const app = express();
+
 const port = process.env.PORT || 3000;
 
-app.get('/prod', (req, res) =>{
+app.get("/prod", (req, res) => {
     res.json([
-        {id: '10',
-            name:"laptop",
+        {
+            id: "10",
+            name: "laptop",
+        },
+    ]);
+});
 
-        }
-    ])
-} )
-
-app.listen(port, () =>{
-    console.log('server on port',port)
-})
+app.listen(port, "0.0.0.0", () => {
+    console.log("server on port", port);
+});
