254ed579d412b705c9af2ebf2032058c36496171
Daniel Calla Castillo <danielcallacastillo7u7@gmail.com>
2026-05-07 15:30:23 -0500

    1

---
+++ index.js
@@ -0,0 +1,16 @@
+import express, { application } from 'express'
+const app = express()
+const port = process.env.PORT || 3000;
+
+app.get('/prod', (req, res) =>{
+    res.json([
+        {id: '10',
+            name:"laptop",
+
+        }
+    ])
+} )
+
+app.listen(port, () =>{
+    console.log('server on port',port)
+})
