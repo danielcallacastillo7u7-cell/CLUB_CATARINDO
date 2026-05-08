ca21325c7ce9612ba5173edce9486b808f019ac7
Daniel Calla Castillo <danielcallacastillo7u7@gmail.com>
2026-05-08 15:05:42 -0500

    Actualización: Backend con CORS

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 import express from "express";
-
+import cors from "cors"; // 1. Importamos CORS
 
 const app = express();
 
+// 2. Habilitamos CORS para que Next.js pueda consultar la API
+app.use(cors());
+app.use(express.json());
 
-const port = process.env.PORT || 3000;
-app.get("/prod", (req, res) => {
+const port = process.env.PORT || 4000;
+
+// Cambié "/prod" a "/products" para que coincida con el front
+app.get("/products", (req, res) => {
     res.json([
         {
-            id: "10",
+            id: 10, // Si en el front usas number, aquí debe ser número
             name: "laptop",
         },
     ]);
 });
 
 app.listen(port, () => {
-    console.log('server on port', port)
-})
+    console.log('🚀 Server ready on port', port);
+});
